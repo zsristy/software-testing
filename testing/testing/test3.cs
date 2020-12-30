1a8fc4d2d27d3023a799acd8ce1c6d1069ea3ede
zsristy <57956603+zsristy@users.noreply.github.com>
2020-12-30 11:01:14 +0600

    Update test3.cs

--- testing/testing/test3.cs
+++ testing/testing/test3.cs
@@ -4,7 +4,7 @@ using System.Text.RegularExpressions;
 using System.Threading;
 using NUnit.Framework;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 
 namespace SeleniumTests
@@ -20,7 +20,7 @@ namespace SeleniumTests
         [SetUp]
         public void SetupTest()
         {
-            driver = new ChromeDriver();
+            driver = new FirefoxDriver();
             baseURL = "https://www.google.com/";
             verificationErrors = new StringBuilder();
         }
